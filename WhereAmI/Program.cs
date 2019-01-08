560745db69886e16991129ac8da6dba4f1037cd2
Nathan Joseph ALard <nathan.alard@nathannatealard@gmail.com>
2019-01-08 22:06:11 +0200

    Added GeoCoordinateWatcher to .net console app

---
+++ WhereAmI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Device.Location;
+
+namespace WhereAmI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting GeoCoordinate Watcher...");
+
+            var watcher = new GeoCoordinateWatcher();
+
+            watcher.StatusChanged += (sender, eventArgs) => {
+                Console.WriteLine($"GeoCoordinateWatcher:StatusChanged:{eventArgs.Status}");
+            };
+
+            watcher.PositionChanged += (sender, eventArgs) => {
+                Console.WriteLine($"GeoCoodinateWatcher:PositionChanged:{eventArgs.Position.Location}");
+            };
+
+            watcher.MovementThreshold = 100; //100 meters of change in position is required for the event to be triggered
+
+            watcher.Start();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
